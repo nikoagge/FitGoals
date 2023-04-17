987d08937193930cc315b7ff9a06d9088aab25b3
nikoagge <n.aggelidis@m-stat.gr>
2023-04-17 17:13:48 +0300

    Initial Commit

---
+++ FitGoals/FitGoalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitGoalsApp.swift
+//  FitGoals
+//
+//  Created by Nikos Aggelidis on 17/4/23.
+//
+
+import SwiftUI
+
+@main
+struct FitGoalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
